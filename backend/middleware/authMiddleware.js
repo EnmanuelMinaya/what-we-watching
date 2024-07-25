69f2320d48f1a27fd9d6c66333c908bc8b33a8d4
Enmanuel <enmanuelminaya@gmail.com>
2024-07-25 19:35:17 -0400

    added routes protection

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import expressAsyncHandler from 'express-async-handler';
+import { PrismaClient } from '@prisma/client';
+
+const protect = expressAsyncHandler(async (req, res, next) => {
+    let token;
+
+    token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await await prisma.user.findUnique({
+                omit: {
+                    password: true,
+                },
+                where: {
+                    id: decoded.userId,
+                },
+            });
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, invalid token')
+        }
+
+    } else {
+        res.status(401);
+        throw new Error('Not authorized')
+    }
+
+})
+
+export { protect };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,7 @@ import {
     updateUserProfile,
     changePassword,
 } from '../../Controller/userController.js';
-
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -15,8 +15,8 @@ const router = express.Router();
 router.post('/auth', authUser);
 router.post('/register', registerUser);
 router.post('/logout', logout);
-router.get('/profile', getUserProfile);
-router.put('/profile', updateUserProfile);
+router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 router.post('/changePassword', changePassword);
 
 
